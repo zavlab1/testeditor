2f6adecfe9fa3ce7200ebeb05421ee377c884496
325aud <325aud@google.com>
2015-12-24 13:56:59 +0400

    Initial commit

--- src/testeditor/TestEditor.java
+++ src/testeditor/TestEditor.java
@@ -14,8 +14,8 @@ public class TestEditor {
 		answ.add(new Answer(true,"Москва"));
 		answ.add(new Answer(false,"Санкт-Петербург"));
 
-		Question qw = new Select("Столица России?",answ);
+		Question q = new Select("Столица России?",answ);
 
-		GiftSaver.toFile("Test.txt",qw);
+		GiftSaver.toFile("Test.txt",q);
 	}
 }
