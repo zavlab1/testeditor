022e528c6d40f179decf13a10a0f4a9dad665260
zavlab1 <zavlab1@gmail.com>
2016-02-24 16:50:20 +0300

    GiftParser 2

--- src/testeditor/parser/GiftParser.java
+++ src/testeditor/parser/GiftParser.java
@@ -4,6 +4,7 @@ import testeditor.Test;
 import testeditor.question.Answer;
 import testeditor.question.Question;
 import testeditor.question.Select;
+import testeditor.saver.Saver;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,28 +20,48 @@ import java.util.Scanner;
 public class GiftParser extends Parser {
     public Test getTest(String filepath) throws IOException {
         Test test = new Test();
-        /*
-        ArrayList<Answer> answers1 = new ArrayList<>();
-        answers1.add(new Answer("Москва", true));
-        answers1.add(new Answer("Санкт-Петербург", false));
-        answers1.add(new Answer("Курск", false));
-
-        Question q1 = new Select("Столица нашей Родины?", answers1);
-        test.add(q1);
-
-        ArrayList<Answer> answers2 = new ArrayList<>();
-        answers2.add(new Answer("ФМФ", false));
-        answers2.add(new Answer("Геофак", false));
-        answers2.add(new Answer("ИПФ", true));
-
-        Question q2 = new Select("Название нашего факультета?", answers2);
-        test.add(q2);*/
-
-        List<String> lineList = new ArrayList<>();
         Scanner in = new Scanner(new File(filepath));
-        while (in.hasNextLine())
-            lineList.add(in.nextLine());
-
+        List<String> lineList = getLineList(in);
+        List<List<String>> qTexts = getQuestionsTexts(in, lineList);
+        for (List<String> qText : qTexts) {
+            Question q = getQuestion(qText);
+            test.add(q);
+        }
         return test;
+    }
+
+    private List<String> getLineList(Scanner in) {
+        List<String> lineList = new ArrayList<>();
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            if (!line.isEmpty()) {
+                lineList.add(line.trim());
+            }
+        }
+        return lineList;
+    }
+
+    private List<List<String>> getQuestionsTexts(Scanner in, List<String> lineList) {
+        List<List<String>> qTexts = new ArrayList<>();
+        for (String line : lineList) {
+            if (line.startsWith("::")) {
+                ArrayList<String> qStrings = new ArrayList<>();
+                qStrings.add(line);
+                String nextLine = null;
+                while ( in.hasNextLine() &&
+                        !( (nextLine = in.nextLine()).startsWith("::")) ) {
+                    qStrings.add(nextLine);
+                }
+                qTexts.add(qStrings);
+            }
+        }
+        return qTexts;
+    }
+
+    private Question getQuestion(List<String> qText) {
+        //здесь будет парсер строк вопроса
+
+
+        return new Question();
     }
 }
