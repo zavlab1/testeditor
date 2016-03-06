aa50f576de46277698e5c819f493322d08cf0d78
Max1998 <fomin-maxim2009@yandex.ru>
2016-03-06 21:31:46 +0300

    XMLParser implementation.

--- src/testeditor/parser/XMLParser.java
+++ src/testeditor/parser/XMLParser.java
@@ -1,36 +1,76 @@
 package testeditor.parser;
 
 import testeditor.Test;
-import testeditor.question.Answer;
-import testeditor.question.Question;
-import testeditor.question.Select;
+import testeditor.question.*;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import testeditor.saver.Saver;
 
 /**
  * Created by dimitry on 28.12.15.
  * Парсер для чтения файлов в формате XML
  */
 public class XMLParser extends Parser {
-    public Test getTest(String filepath) {
-        Test test = new Test();
+    Document doc;
+    Test resultTest;
 
-        ArrayList<Answer> answers1 = new ArrayList<>();
-        answers1.add(new Answer("Москва", true));
-        answers1.add(new Answer("Санкт-Петербург", false));
-        answers1.add(new Answer("Курск", false));
+    public Test getTest(String filepath) throws IOException {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            doc = builder.parse(filepath);
+        }
 
-        Question q1 = new Select(1, "Столица нашей Родины?", answers1);
-        test.add(q1);
+        catch (Exception e) { throw new IOException("Ошибка открытия XML файла: " + e.toString()); }
 
-        ArrayList<Answer> answers2 = new ArrayList<>();
-        answers2.add(new Answer("ФМФ", false));
-        answers2.add(new Answer("Геофак", false));
-        answers2.add(new Answer("ИПФ", true));
+        resultTest = new Test();
+        Element documentElement = doc.getDocumentElement();
+        NodeList questionNodes = doc.getChildNodes();
 
-        Question q2 = new Select(2, "Название нашего факультета?", answers2);
-        test.add(q2);
+        for(int n = 0; n < questionNodes.getLength(); n++) {
+            Element questionElement = (Element)questionNodes.item(0);
 
-        return test;
+            try {
+                resultTest.add(getQuestion(questionElement, n));
+            }
+
+            catch (Exception e) {
+            }
+        }
+
+        return resultTest;
+    }
+
+    public Question getQuestion(Element questionElement, int Number) throws Exception {
+        NodeList answerElements = questionElement.getChildNodes();
+        ArrayList<Answer> answers = new ArrayList<>();
+
+        for(int n = 0; n < answerElements.getLength(); n++) {
+            Element answerElement = (Element)answerElements.item(n);
+            answers.add(new Answer(answerElement.getAttribute("Name"),
+                    Boolean.parseBoolean(answerElement.getAttribute("Right"))));
+        }
+
+        switch (questionElement.getAttribute("Type")) {
+            case "Order":
+                return new Order(Number, questionElement.getAttribute("Head"),
+                        answers);
+            case "Select":
+                return new Select(Number, questionElement.getAttribute("Head"),
+                        answers);
+            case "Conformity":
+                return new Conformity(Number, questionElement.getAttribute("Head"),
+                        answers);
+            case "TrueFalse":
+                return new TrueFalse(Number, questionElement.getAttribute("Head"),
+                        answers);
+            default:
+                throw new Exception("Неправильный тип вопроса.");
+        }
     }
 }
