56308f4515e6b766f25200b7db34a0377c59e7e4
sergmaks <maximov.sv90@mail.ru>
2016-03-19 02:01:55 +0400

    GUI Beginning (Questions' Heads Output)

--- src/testeditor/TestEditor.java
+++ src/testeditor/TestEditor.java
@@ -2,12 +2,16 @@ package testeditor;
 
 import testeditor.question.*;
 import testeditor.saver.*;
+import testeditor.Interface.*;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.PrintStream;
 
 
 public class TestEditor {
     public static void main(String[] args) {
+        /*
         Test test =	Test.getTestFromFile("test.gift");
         Saver s = new GiftSaver(test, "Test1.gift");
         for (Question q : test) {
@@ -17,5 +21,17 @@ public class TestEditor {
             break; // только для тестирования. Т.к. для сохранения даже одного вопроса файл переписывается полностью,
                    // то нет смысла гонять весь цикл.
         }
+        */
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame testFrame = new MainFrame();
+
+                testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                testFrame.setTitle("TestEditor");
+                testFrame.setVisible(true);
+            }
+        });
+
     }
 }
