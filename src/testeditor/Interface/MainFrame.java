d143dffe34eb2d84a7b09010c79fa9d9b553a0f6
sergmaks <maximov.sv90@mail.ru>
2016-03-19 10:46:48 +0400

    GUI Beginning (Questions' Heads Output)

--- src/testeditor/Interface/MainFrame.java
+++ src/testeditor/Interface/MainFrame.java
@@ -4,12 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import testeditor.Interface.Actions.*;
+<<<<<<< HEAD
 import testeditor.question.Question;
+=======
+>>>>>>> 4e6b005... GUI Beginning (Questions' Heads Output)
 
 /**
  * Главное окно
  */
 public class MainFrame extends JFrame {
+<<<<<<< HEAD
+=======
+    private JPanel controlPanel;
+    private JButton openButton;
+    private JTextArea content; // содержимое теста
+    private JMenuBar menuBar; // меню
+>>>>>>> 4e6b005... GUI Beginning (Questions' Heads Output)
 
     public MainFrame(){
         // определяем размер экрана
@@ -26,6 +36,7 @@ public class MainFrame extends JFrame {
         this.setIconImage(new ImageIcon("src/testeditor/Interface/img/main.png").getImage());// путь к файлу нужно указывать не относительно текущего пакета, а относительно корня проекта
 
         //------- Создаем и настраиваем компоненты GUI -------//
+<<<<<<< HEAD
         DefaultListModel<Question> listModel = new DefaultListModel<>();// Модель для компонета contentList
         JList<Question> questionList = new JList(listModel); // Output List
         questionList.setBackground(Color.GRAY);
@@ -47,6 +58,24 @@ public class MainFrame extends JFrame {
 
         JMenuItem openMenu = new JMenuItem(new OpenAction(listModel));
         openMenu.setAccelerator(KeyStroke.getKeyStroke("ctrl O"));// оперативные клавиши
+=======
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout(FlowLayout.RIGHT,30,30));
+
+        openButton = new JButton(new OpenAction(content));
+        controlPanel.add(openButton);
+
+        content = new JTextArea();
+        content.setLineWrap(true);//автоматический перенос строк
+
+        JScrollPane scrollPane = new JScrollPane(content); // полоса прокрутки для TextArea
+
+        //------- Создаем главное меню -------//
+        menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem openMenu = new JMenuItem(new OpenAction(content));
+>>>>>>> 4e6b005... GUI Beginning (Questions' Heads Output)
         fileMenu.add(openMenu);
 
         JMenuItem exitMenu = new JMenuItem("Exit");
@@ -56,6 +85,14 @@ public class MainFrame extends JFrame {
 
         menuBar.add(fileMenu);
 
+<<<<<<< HEAD
         this.setJMenuBar(menuBar);
+=======
+        //-------- Добавляем на форму созданные компоненты GUI -------//
+        this.setJMenuBar(menuBar);
+        //add(content); - scrollPane является контейнером дя TextArea, поэтому нужно добавлять только его
+        this.add(scrollPane);
+        this.add(controlPanel,BorderLayout.SOUTH);
+>>>>>>> 4e6b005... GUI Beginning (Questions' Heads Output)
     }
 }
