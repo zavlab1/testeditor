0c0e1a1c9f226dbaec19645399e9de42b5d8ba6e
sergmaks <maximov.sv90@mail.ru>
2016-03-20 00:12:14 +0400

    GUI using the JList

--- src/testeditor/Interface/MainFrame.java
+++ src/testeditor/Interface/MainFrame.java
@@ -9,10 +9,6 @@ import testeditor.Interface.Actions.*;
  * Главное окно
  */
 public class MainFrame extends JFrame {
-    private JPanel controlPanel;
-    private JButton openButton;
-    private JTextArea content; // содержимое теста
-    private JMenuBar menuBar; // меню
 
     public MainFrame(){
         // определяем размер экрана
@@ -29,22 +25,24 @@ public class MainFrame extends JFrame {
         this.setIconImage(new ImageIcon("src/testeditor/Interface/img/main.png").getImage());// путь к файлу нужно указывать не относительно текущего пакета, а относительно корня проекта
 
         //------- Создаем и настраиваем компоненты GUI -------//
-        controlPanel = new JPanel();
+        DefaultListModel listModel = new DefaultListModel();// Модель для компонета contentList
+        JList contentList = new JList(listModel); // Output List
+        JScrollPane scrollPane = new JScrollPane(contentList); // полоса прокрутки для списка
+        this.add(scrollPane);
+
+        JPanel controlPanel = new JPanel(); // Панель с кнопками
         controlPanel.setLayout(new FlowLayout(FlowLayout.RIGHT,30,30));
 
-        openButton = new JButton(new OpenAction(content));
+        JButton openButton = new JButton(new OpenAction(listModel));
         controlPanel.add(openButton);
-
-        content = new JTextArea();
-        content.setLineWrap(true);//автоматический перенос строк
-
-        JScrollPane scrollPane = new JScrollPane(content); // полоса прокрутки для TextArea
+        this.add(controlPanel,BorderLayout.SOUTH);
 
         //------- Создаем главное меню -------//
-        menuBar = new JMenuBar();
+        JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
 
-        JMenuItem openMenu = new JMenuItem(new OpenAction(content));
+        JMenuItem openMenu = new JMenuItem(new OpenAction(listModel));
+        openMenu.setAccelerator(KeyStroke.getKeyStroke("ctrl O"));// оперативные клавиши
         fileMenu.add(openMenu);
 
         JMenuItem exitMenu = new JMenuItem("Exit");
@@ -54,10 +52,6 @@ public class MainFrame extends JFrame {
 
         menuBar.add(fileMenu);
 
-        //-------- Добавляем на форму созданные компоненты GUI -------//
         this.setJMenuBar(menuBar);
-        //add(content); - scrollPane является контейнером дя TextArea, поэтому нужно добавлять только его
-        this.add(scrollPane);
-        this.add(controlPanel,BorderLayout.SOUTH);
     }
 }
