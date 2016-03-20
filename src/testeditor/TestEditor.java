46d39ba19adcae3890192ee78acb1077b8137c9e
zavlab1 <zavlab1@gmail.com>
2016-03-21 00:34:55 +0300

    Change varnames and title type (changed)

--- src/testeditor/TestEditor.java
+++ src/testeditor/TestEditor.java
@@ -11,8 +11,10 @@ import java.io.PrintStream;
 
 public class TestEditor {
     public static void main(String[] args) {
+
         /*
         Test test =	Test.getTestFromFile("test.gift");
+
         Saver s = new GiftSaver(test, "Test1.gift");
         for (Question q : test) {
 
