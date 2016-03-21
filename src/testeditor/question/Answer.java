b8dffb9d98a2aed0eca9c6d1f68b32baa0769c55
zavlab1 <zavlab1@gmail.com>
2016-03-21 18:59:16 +0300

    Small fixes and optimization

--- src/testeditor/question/Answer.java
+++ src/testeditor/question/Answer.java
@@ -6,47 +6,21 @@ package testeditor.question;
  */
 public class Answer {
 
-<<<<<<< HEAD
-    public static final int MAX_DEGREE = 100;
-    public static final int MIN_DEGREE = 0;
-
-    private int degree;
-    private String aText;
-    private String aComment;
-=======
     private int degree;
     private String value;
->>>>>>> 836a764... Do degree for answer int type & small fixes
 
     /**
-     * @param text - значение варианта ответа
      * @param degree - указывает на степень правильности варианта ответа (от 0 до 1 с точностью до сотых)
-     * @param comment - Комментарий (если есть)
+     * @param value - значение варианта ответа
      */
-<<<<<<< HEAD
-    public Answer(String text, int degree, String comment) {
-        aText = text;
-        aComment = comment;
-=======
     public Answer(String value, int degree) {
         this.value = value;
->>>>>>> 836a764... Do degree for answer int type & small fixes
         this.degree = degree;
-    }
-    public Answer(String text, int degree) {
-        this(text, degree, "");
-    }
-    public Answer(String text) {
-        this(text, Answer.MAX_DEGREE, "");
     }
 
     /**
-     * @return возвращает строку с вариантом ответа
+     * @return возвращает строку с КОНКРЕТНЫМ вариантом ответа
      */
-<<<<<<< HEAD
-    public String getAText() {
-        return this.aText;
-=======
     public String getValue() {
         return this.value;
     }
@@ -54,35 +28,16 @@ public class Answer {
     public void setValue(String value, int degree) {
         this.value = value;
         this.degree = degree;
->>>>>>> 836a764... Do degree for answer int type & small fixes
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 
     /**
-     * @return возвращает строку с комментарием для вариантом ответа
-     */
-    public String getAComment() {
-        return this.aComment;
-    }
-
-    /**
-     * @return возвращает правильность варианта в процентах
+     * @return возвращает правильность варианта
      */
     public int getDegree() {
         return degree;
-    }
-    public void setValue (String text, int degree, String comment) {
-        aText = text;
-        aComment = comment;
-        this.degree = degree;
-    }
-
-    public void setAText(String aText) {
-        this.aText = aText;
-    }
-    public void setDegree(int degree) {
-        this.degree = degree;
-    }
-    public void setAComment(String aComment) {
-        this.aComment = aComment;
     }
 }
