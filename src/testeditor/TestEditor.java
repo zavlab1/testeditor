af0b59327aa8217a92ae138938aafe848fdca73f
zavlab1 <zavlab1@gmail.com>
2016-03-21 18:59:16 +0300

    Small fixes and optimization

--- src/testeditor/TestEditor.java
+++ src/testeditor/TestEditor.java
@@ -12,18 +12,16 @@ import java.io.PrintStream;
 public class TestEditor {
     public static void main(String[] args) {
 
-        /*
-        Test test =	Test.getTestFromFile("test.gift");
-
+        Test test = Test.getTestFromFile("test.gift");
         Saver s = new GiftSaver(test, "Test1.gift");
         for (Question q : test) {
 
-                s.save(q);
+            s.save(q);
 
             break; // только для тестирования. Т.к. для сохранения даже одного вопроса файл переписывается полностью,
-                   // то нет смысла гонять весь цикл.
+            // то нет смысла гонять весь цикл.
         }
-        */
+     /*
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -33,6 +31,6 @@ public class TestEditor {
                 testFrame.setTitle("TestEditor");
                 testFrame.setVisible(true);
             }
-        });
+        });*/
     }
 }
