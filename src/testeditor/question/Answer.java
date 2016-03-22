0fbdfe85af1ce278e00299015f7a10fd28c2b340
zavlab1 <zavlab1@gmail.com>
2016-03-23 00:01:05 +0300

    Refactoring GiftParser. Add comments to Answer

--- src/testeditor/question/Answer.java
+++ src/testeditor/question/Answer.java
@@ -14,42 +14,55 @@ public class Answer {
     private String aComment;
 
     /**
+     * @param text - значение варианта ответа
      * @param degree - указывает на степень правильности варианта ответа (от 0 до 1 с точностью до сотых)
-     * @param text - значение варианта ответа c комментарием (если есть)
+     * @param comment - Комментарий (если есть)
      */
-    public Answer(String text, int degree) {
-        String[] value = text.split("#");
-        this.aText = value[0];
-        this.aComment = value.length == 2 ? value[1] : "";
+    public Answer(String text, int degree, String comment) {
+        aText = text;
+        aComment = comment;
         this.degree = degree;
     }
+    public Answer(String text, int degree) {
+        this(text, degree, "");
+    }
     public Answer(String text) {
-        this(text, Answer.MAX_DEGREE);
+        this(text, Answer.MAX_DEGREE, "");
     }
 
     /**
-     * @return возвращает строку с КОНКРЕТНЫМ вариантом ответа
+     * @return возвращает строку с вариантом ответа
      */
     public String getAText() {
         return this.aText;
     }
+
+    /**
+     * @return возвращает строку с комментарием для вариантом ответа
+     */
     public String getAComment() {
         return this.aComment;
     }
+
     /**
      * @return возвращает правильность варианта в процентах
      */
     public int getDegree() {
         return degree;
     }
-    public void setValue (String text, int degree) {
-        String[] value = text.split("#");
-        this.aText = value[0];
-        this.aComment = value.length == 2 ? value[1] : "";
+    public void setValue (String text, int degree, String comment) {
+        aText = text;
+        aComment = comment;
         this.degree = degree;
     }
 
     public void setAText(String aText) {
         this.aText = aText;
+    }
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+    public void setAComment(String aComment) {
+        this.aComment = aComment;
     }
 }
