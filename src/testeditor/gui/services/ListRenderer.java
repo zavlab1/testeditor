4195ef3e6194889620a1f40edd67e476d0c5b871
sergmaks <maximov.sv90@mail.ru>
2016-03-30 22:19:23 +0300

    Output question Type (cherry picked from commit 1f51e0c)

--- src/testeditor/gui/services/ListRenderer.java
+++ src/testeditor/gui/services/ListRenderer.java
@@ -1,8 +1,6 @@
 package testeditor.gui.services;
 
 import javax.swing.*;
-
-import sun.plugin.javascript.navig.Anchor;
 import testeditor.question.*;
 
 import java.awt.*;
@@ -13,25 +11,25 @@ import java.awt.*;
 public class ListRenderer extends JPanel implements ListCellRenderer<Question> {
     @Override
     public Component getListCellRendererComponent(JList<? extends Question> list, Question value, int index, boolean isSelected, boolean cellHasFocus) {
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout(20,20));
         setBackground(isSelected ? new Color(252,252,252): new Color(230,230,230));
 
         removeAll();
 
         JLabel labelQuestion = new JLabel("<html>" +
-                "<b>"+value.getQName()+"</b>" +
+                "<br><b>"+value.getQName()+"</b>" +
                 "<br>"+value.getQText()+
                 "</html>");
         labelQuestion.setFont(new Font("Serif",Font.PLAIN,12));
-        add(labelQuestion,new GBC(0,0,1,1,0,0,100,0).setFill(GBC.HORIZONTAL).setInsets(10,10,10,20));
+        add(labelQuestion,"West");
 
         JLabel labelType = new JLabel("<html>" +
-                "<b>Тип вопроса:</b>" +
-                "<br>" + value.TYPE + "</html>");
+                "<br><b>Тип вопроса:</b>" +
+                "<br>"+value.getType()+"</html>");
         labelType.setFont(new Font("Serif",Font.PLAIN,12));
-        add(labelType,new GBC(2,0,1,1,0,0,0,0).setFill(GBC.HORIZONTAL).setInsets(10,20,10,10));
+        add(labelType,"East");
 
-       add(new JSeparator(),new GBC(0,1,3,1,0,0,100,0).setFill(GBC.HORIZONTAL));
+        add(new JSeparator(),"South");
 
         return this;
     }
