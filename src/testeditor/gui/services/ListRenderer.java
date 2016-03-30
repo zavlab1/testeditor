4d28f6296040aca30b3f1d798c2f4f63f8b48a4d
325aud <325aud@google.com>
2016-03-30 13:48:33 +0400

    Question output beginning

--- src/testeditor/gui/services/ListRenderer.java
+++ src/testeditor/gui/services/ListRenderer.java
@@ -11,12 +11,24 @@ import java.awt.*;
 public class ListRenderer extends JPanel implements ListCellRenderer<Question> {
     @Override
     public Component getListCellRendererComponent(JList<? extends Question> list, Question value, int index, boolean isSelected, boolean cellHasFocus) {
-        setLayout(new BorderLayout(30,30));
+        setLayout(new BorderLayout(20,20));
         setBackground(isSelected ? new Color(252,252,252): new Color(230,230,230));
-        JLabel label = new JLabel("    "+value.getQName());
 
         removeAll();
-        add(label,"West");
+
+        JLabel labelQuestion = new JLabel("<html>" +
+                "<br><b>"+value.getQName()+"</b>" +
+                "<br>"+value.getQText()+
+                "</html>");
+        labelQuestion.setFont(new Font("Serif",Font.PLAIN,12));
+        add(labelQuestion,"West");
+
+        JLabel labelType = new JLabel("<html>" +
+                "<br><b>Тип вопроса: </b>" +
+                "<br>Type</html>");
+        labelType.setFont(new Font("Serif",Font.PLAIN,12));
+        add(labelType,"East");
+
         add(new JSeparator(),"South");
 
         return this;
