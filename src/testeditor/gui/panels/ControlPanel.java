4c4c3343ff0db20a707e4437c5a877dda2bfb688
sergmaks <maximov.sv90@mail.ru>
2016-03-30 22:19:23 +0300

    Output question Type (cherry picked from commit 1f51e0c)

--- src/testeditor/gui/panels/ControlPanel.java
+++ src/testeditor/gui/panels/ControlPanel.java
@@ -15,20 +15,20 @@ import java.awt.*;
 public class ControlPanel extends JPanel {
     public ControlPanel(DefaultListModel<Question> listModel){
 
-        setLayout(new GridBagLayout());
+        setLayout(new FlowLayout(FlowLayout.LEFT,20,20));
 
         JButton createButton = new VerticalButton(new CreateAction());//кнопка создать тест
         createButton.setEnabled(false);
-        add(createButton,new GBC(0,0,1,1,10,10,1,0).setFill(GridBagConstraints.HORIZONTAL).setInsets(20,0,20,20));
+        add(createButton);
 
         JButton openButton = new VerticalButton(new OpenAction(listModel));// кнопка открыть тест
-        add(openButton,new GBC(1,0,1,1,10,10,1,0).setFill(GridBagConstraints.HORIZONTAL).setInsets(20,0,20,20));
+        add(openButton);
 
         JButton saveAsButton = new VerticalButton(new SaveAction());// кнопка сохранить как
-        add(saveAsButton,new GBC(2,0,1,1,10,10,1,0).setFill(GridBagConstraints.HORIZONTAL).setInsets(20,0,20,20));
+        add(saveAsButton);
         saveAsButton.setEnabled(false);
 
         JPanel emptyPanel = new JPanel(); // пустая панель для сдвига кнопок влево
-        add(emptyPanel, new GBC(3,0,1,1,10,10,1,0).setFill(GridBagConstraints.HORIZONTAL).setInsets(20,20,20,20));
+        add(emptyPanel);
     }
 }
