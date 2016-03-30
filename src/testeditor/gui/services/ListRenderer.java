18e0ddc1325c368c63311ad75f690f6182452752
325aud <325aud@google.com>
2016-03-30 11:25:34 +0300

    Updated control panels (cherry picked from commit 66a7387)

--- src/testeditor/gui/services/ListRenderer.java
+++ src/testeditor/gui/services/ListRenderer.java
@@ -11,24 +11,12 @@ import java.awt.*;
 public class ListRenderer extends JPanel implements ListCellRenderer<Question> {
     @Override
     public Component getListCellRendererComponent(JList<? extends Question> list, Question value, int index, boolean isSelected, boolean cellHasFocus) {
-        setLayout(new BorderLayout(20,20));
+        setLayout(new BorderLayout(30,30));
         setBackground(isSelected ? new Color(252,252,252): new Color(230,230,230));
+        JLabel label = new JLabel("    "+value.getQName());
 
         removeAll();
-
-        JLabel labelQuestion = new JLabel("<html>" +
-                "<br><b>"+value.getQName()+"</b>" +
-                "<br>"+value.getQText()+
-                "</html>");
-        labelQuestion.setFont(new Font("Serif",Font.PLAIN,12));
-        add(labelQuestion,"West");
-
-        JLabel labelType = new JLabel("<html>" +
-                "<br><b>Тип вопроса:</b>" +
-                "<br>"+value.getType()+"</html>");
-        labelType.setFont(new Font("Serif",Font.PLAIN,12));
-        add(labelType,"East");
-
+        add(label,"West");
         add(new JSeparator(),"South");
 
         return this;
