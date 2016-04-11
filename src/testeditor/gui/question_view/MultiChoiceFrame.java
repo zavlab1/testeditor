05779772b4b49b693adbe9b608d22a97fbbceab4
325aud <325aud@google.com>
2016-04-11 15:03:00 +0400

    Degree handling for MultiChoiceFrame

--- src/testeditor/gui/question_view/MultiChoiceFrame.java
+++ src/testeditor/gui/question_view/MultiChoiceFrame.java
@@ -19,8 +19,9 @@ import java.util.List;
  */
 public class MultiChoiceFrame extends QuestionFrame {
     List<Answer> aList;
+    List<JCheckBox> checkBoxList = new ArrayList<>();
+    List<JSpinner> spinnerList = new ArrayList<>();
     JPanel answers = new JPanel();
-    int maxDegree = 0;
     int aCount;
     int offset = 0;
 
@@ -42,14 +43,15 @@ public class MultiChoiceFrame extends QuestionFrame {
         answers.add(weightLabel, new GBC(6,0,1,1,0,0,0,0).setFill(GBC.HORIZONTAL).setInsets(0,5,0,5));
         answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(7,0,1,1,0,0,0,0).setFill(GBC.VERTICAL));
 
-
         aList = q.getAnswerList();
         aCount = aList.size();
 
-        for(int i=0; i<aCount; i++) {
+        for(int i=0; i < aCount; i++) {
             offset++;
             addAnswer( i + 1, aList.get(i).getAText(), aList.get(i).getAComment(),
                     aList.get(i).getDegree());
+
+            updateAnswers();
         }
 
         JButton addButton = new JButton("<html><font color='green' size=+1>&nbsp;<b>&#10010;&nbsp;</b></font>Добавить&nbsp;</html>");
@@ -58,50 +60,58 @@ public class MultiChoiceFrame extends QuestionFrame {
 
         JPanel addButtonPanel = new JPanel(new FlowLayout());
         addButtonPanel.add(addButton);
-        addButton.addActionListener((ActionEvent e) -> {
-            addAnswer(aCount+1,"","",0);
-            answers.updateUI();
-            aCount++;
-            aScrollPane.setViewportView(answerPanel);
-            offset++;
-        });
+        addButton.addActionListener(e -> {
+                                            addAnswer(aCount+1,"","",0);
+                                            answers.updateUI();
+                                            aCount++;
+                                            aScrollPane.setViewportView(answerPanel);
+                                            offset++;
+                                            updateAnswers();
+                                          });
 
         answerPanel.add(addButtonPanel, BorderLayout.CENTER);
     }
 
     public void addAnswer (int pos, String text, String comment, int degree){
 
-        JRadioButton b = new JRadioButton();
-        answers.add(b, new GBC(0, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.HORIZONTAL).setInsets(10, 5, 10, 10));
-        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(1,pos,1,1,0,0,0,0).setFill(GBC.VERTICAL));
+        JCheckBox check = new JCheckBox();
+        check.setSelected(degree > 0);
+        checkBoxList.add(check);
+
+        check.addChangeListener(event -> updateAnswers());
+
+        answers.add(check, new GBC(0, pos, 1, 1, 0, 0, 0, 0).setInsets(10, 5, 10, 10));
+        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(1, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.VERTICAL));
 
         QTextArea answerText = new QTextArea(text);
         answerText.setLineWrap(true);
         fields.add(answerText);
         answers.add(answerText, new GBC(2, pos, 1, 1, 0, 0, 100, 0).setFill(GBC.BOTH).setInsets(5, 5, 5, 5));
-        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(3,pos,1,1,0,0,0,0).setFill(GBC.VERTICAL));
+        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(3, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.VERTICAL));
 
         QTextArea commentText = new QTextArea(comment);
         commentText.setLineWrap(true);
-        answers.add(commentText, new GBC(4, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.HORIZONTAL).setInsets(2, 5, 5, 5));
-        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(5,pos,1,1,0,0,0,0).setFill(GBC.VERTICAL));
+        answers.add(commentText, new GBC(4, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.BOTH).setInsets(5, 5, 5, 5));
+        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(5, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.VERTICAL));
 
-       JSpinner degreeSpinner = new JSpinner(new SpinnerNumberModel(0,0,100,1));
+        SpinnerNumberModel spinnerNumberModel = new SpinnerNumberModel(0,0,100,1);
+
+        JSpinner degreeSpinner = new JSpinner(spinnerNumberModel);
         degreeSpinner.setValue(degree);
-        degreeSpinner.addChangeListener((ChangeEvent event) -> {
-               // degreeSpinner.setModel(new SpinnerNumberModel(0,0,100 - maxDegree,1));
 
+        degreeSpinner.addChangeListener(event -> {
+            int sumDegree = spinnerList.stream().mapToInt(x -> x.isEnabled() ? (int)x.getValue() : 0).sum();
+            System.out.println(sumDegree);
         });
-        answers.add(degreeSpinner,new GBC(6, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.HORIZONTAL).setInsets(2, 5, 5, 5));
-        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(7,pos,1,1,0,0,0,0).setFill(GBC.VERTICAL));
+
+        spinnerList.add(degreeSpinner);
+        answers.add(degreeSpinner,new GBC(6, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.HORIZONTAL).setInsets(5, 5, 5, 5));
+        answers.add(new JSeparator(JSeparator.VERTICAL), new GBC(7, pos, 1, 1, 0, 0, 0, 0).setFill(GBC.VERTICAL));
 
         JButton delButton = new JButton("<html><font color='red'><b>&nbsp;&#10006;&nbsp;</b></font></html>");
         delButton.addActionListener((ActionEvent e) -> deleteAnswer(answers.getComponentZOrder(delButton)));
         answers.add(delButton, new GBC(8, pos, 1, 1, 0, 0, 0, 0).setInsets(5, 10, 5, 5));
-        answers.add(new JSeparator(), new GBC(0, pos + 1, 9, 1, 0, 0, 0, 0).setFill(GBC.BOTH).setInsets(0,0,5,0));
-
-        if (maxDegree <= 100) maxDegree += degree;
-        System.out.println(maxDegree);
+        answers.add(new JSeparator(), new GBC(0, pos + 1, 9, 1, 0, 0, 0, 0).setFill(GBC.BOTH).setInsets(0, 0, 5, 0));
     }
 
     public void deleteAnswer (int index){
@@ -112,16 +122,30 @@ public class MultiChoiceFrame extends QuestionFrame {
         offset--;
     }
 
+    // Обновление панели вариантов, в зависимости от их весов
+    public void updateAnswers(){
+        int countSelected = 0;
+
+        for (JCheckBox checkBox : checkBoxList)
+            countSelected = (checkBox.isSelected()) ? countSelected + 1 : countSelected;
+
+        if (countSelected < 2)
+            spinnerList.stream().forEach(s -> s.setEnabled(false));
+        else
+            for (int i = 0; i < spinnerList.size(); i++)
+                spinnerList.get(i).setEnabled(checkBoxList.get(i).isSelected());
+    }
+
     protected List<Answer> collectAnswers() {
         List<Answer> aList = new ArrayList<>();
-		Answer answer;
-		QTextArea tempTextArea;
+        Answer answer;
+        QTextArea tempTextArea;
 
-		for (int i = 1; i < answers.getComponentCount(); i+=7){
-			tempTextArea = (QTextArea)answers.getComponent(i);
-			answer = new Answer(tempTextArea.getText());
-			aList.add(answer);
-		}
+        for (int i = 1; i < answers.getComponentCount(); i+=7){
+            tempTextArea = (QTextArea)answers.getComponent(i);
+            answer = new Answer(tempTextArea.getText());
+            aList.add(answer);
+        }
 
         return aList;
     }
